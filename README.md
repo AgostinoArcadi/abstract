5fd520868177029102718ad6d4517c4c98cac70f
Agostino <agostinoarcadi@gmail.com>
2023-11-21 12:34:18 +0100

    Modificato la classe Forma in abstract

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# abstract
+﻿# abstract
+- Scrivere un programma che contenga una classe astratta chiamata Forma
+ed un metodo astratto chiamato calcolaArea().
+
+- Crea poi due sottoclassi Rettangolo e Triangolo che estendono Forma
+ed implementano il metodo per il calcolo dell'area in maniera specifica.
---
+++ src/Forma.java
@@ -0,0 +1,28 @@
+public abstract class Forma {
+    private int base;
+
+    private int altezza;
+
+    public Forma(int base, int altezza) {
+        this.base = base;
+        this.altezza = altezza;
+    }
+
+    public int getBase() {
+        return base;
+    }
+
+    public void setBase(int base) {
+        this.base = base;
+    }
+
+    public int getAltezza() {
+        return altezza;
+    }
+
+    public void setAltezza(int altezza) {
+        this.altezza = altezza;
+    }
+
+    public abstract void calcolaArea();
+}
